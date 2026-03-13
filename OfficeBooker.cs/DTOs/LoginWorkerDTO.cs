864685267922005c446cd3a145204e5919d9513a
Filip <filipmirzejewski1@gmail.com>
2026-03-13 10:53:11 +0100

    feat:add LoginWorkerDTO and implement WorkerDTO class

---
+++ OfficeBooker.cs/DTOs/LoginWorkerDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OfficeBooker.Models.cs.DTOs
+{
+    public  class LoginworkerDTO
+    {
+        [Required]
+        public string Email { get; set; } = default!;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = default!;
+        
+    }
+}
--- OfficeBooker.cs/DTOs/WorkerDTO.cs
+++ OfficeBooker.cs/DTOs/WorkerDTO.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+using System.ComponentModel.DataAnnotations;
+
 
 namespace OfficeBooker.Models.cs.DTOs
 {
-    internal class WorkerDTO
+    public class WorkerDTO
     {
+        public string Id { get; set; } = default!;
+        [Required]
+        public string Name { get; set; } = default!;
+        [Required]
+        public string Surname { get; set; } = default!;
+        [Required]
+        public string Email { get; set; } = default!;
+        public List<int> ReservationId { get; set; } = new List<int>();
     }
 }
