d0637ebeab7b890bf1661422b22e246ee546ad52
Filip <filipmirzejewski1@gmail.com>
2026-03-16 20:04:36 +0100

    feat: add authService

---
+++ OfficeBooker.Services/IServices/AuthService.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using OfficeBooker.Models.cs;
+using OfficeBooker.Models.cs.DTOs;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace OfficeBooker.Services.IServices
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<Worker> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+        public AuthService(UserManager<Worker> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+        public async Task<object?> GetCurrentUserAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) return null;
+
+            return new
+            {
+                user.Email,
+                user.Name,
+                user.Surname,
+                Roles = await _userManager.GetRolesAsync(user)
+            };
+        }
+
+        public async Task<AuthResponseDTO?> LoginAsync(LoginWorkerDTO model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null || !await _userManager.CheckPasswordAsync(user, model.Password)) return null;
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+            var authClaims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Email, user.Email!),
+            new Claim(ClaimTypes.NameIdentifier, user.Id),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        };
+            foreach (var role in userRoles) authClaims.Add(new Claim(ClaimTypes.Role, role));
+
+            var authSignKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                expires: DateTime.Now.AddDays(1),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSignKey, SecurityAlgorithms.HmacSha256)
+            );
+
+            return new AuthResponseDTO
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = token.ValidTo
+            };
+        }
+
+        public async Task<(bool Success, string Message)> RegisterAsync(CreateWorkerDTO model)
+        {
+            var userExist = await _userManager.FindByEmailAsync(model.Email);
+            if (userExist != null) return (false, "Użytkownik już istnieje.");
+
+            var worker = new Worker { UserName = model.Email, Name = model.Name, Surname = model.Surname, Email = model.Email };
+            var result = await _userManager.CreateAsync(worker, model.Password);
+
+            if (result.Succeeded)
+            {
+                if (!await _roleManager.RoleExistsAsync(model.Role))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(model.Role));
+                }
+                await _userManager.AddToRoleAsync(worker, model.Role);
+                return (true, "Registration Successfu");
+            }
+            return (false, string.Join(", ", result.Errors.Select(e => e.Description)));
+        }
+    }
+}
