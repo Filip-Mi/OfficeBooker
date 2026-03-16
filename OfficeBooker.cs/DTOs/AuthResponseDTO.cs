be7550dc028a6927e668962dd7aefe51de0c7675
Filip <filipmirzejewski1@gmail.com>
2026-03-16 19:30:34 +0100

    add AuthResponseDTO model

---
+++ OfficeBooker.cs/DTOs/AuthResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OfficeBooker.Models.cs.DTOs
+{
+    public class AuthResponseDTO
+    {
+        public string Token { get; set; } = string.Empty;
+        public DateTime Expiration { get; set; }
+    }
+}
