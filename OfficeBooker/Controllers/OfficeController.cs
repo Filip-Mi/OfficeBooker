541b9ada9218607ac02e22a9d74ef43a3dc0e9d1
Filip <filipmirzejewski1@gmail.com>
2026-03-16 17:42:30 +0100

    feat/ OfficeController , Get and Post methods

--- OfficeBooker/Controllers/OfficeController.cs
+++ OfficeBooker/Controllers/OfficeController.cs
@@ -1,16 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using OfficeBooker.DataAccess.Repository.I_Repository;
+using OfficeBooker.Models;
+using OfficeBooker.Models.DTOs;
 
 namespace OfficeBooker.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class OfficeController : ControllerBase
     {
         private readonly IUnitOfWork _unitOfWork;
         public OfficeController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OfficeDTO>>> GetOffices()
+        {
+            var offices = await _unitOfWork.officeRepository.GetAll();
+
+            var officesDTO = offices.Select(o => new OfficeDTO { Id = o.Id, OfficeNumber = o.OfficeNumber, Capacity = o.Capacity , FloorNumber = o.FloorNumber , Equipment = o.Equipment}).ToList;
+            return Ok(officesDTO);
+        }
+        [HttpPost]
+        [Authorize(Roles ="Admin")]
+        public async Task<IActionResult> CreateOffice([FromBody]OfficeDTO officeDTO)
+        {
+            var office = new Office
+            {
+                FloorNumber = officeDTO.FloorNumber,
+                Capacity = officeDTO.Capacity,
+                OfficeNumber = officeDTO.OfficeNumber,
+                Equipment = officeDTO.Equipment
+            };
+            _unitOfWork.officeRepository.Add(office);
+            _unitOfWork.Save();
+            return Ok(office);
+
         }
 
     }
