affd983370d3f8c01e318b6f5b28e2ce8f5b65b1
Filip-Mi <filipmirzejewski1@gmail.com>
2026-03-19 12:11:12 +0100

    feat: add ReservationRespondDTO

---
+++ OfficeBooker.cs/DTOs/ReservationRespondDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OfficeBooker.Models.cs.DTOs
+{
+    public class ReservationRespondDTO
+    {
+        [Required]
+        public int Id { get; init; }
+        [Required]
+        public string WorkerId { get; init; } = string.Empty;
+        [Required]
+        public DateTime ReservationStartTime { get; init; }
+        [Required]
+        public DateTime ReservationEndTime { get; init; }
+        [Required]
+        public DateTime ReservationCreateTime { get; init; }
+        [Required]
+        public int OfficeId { get; init; }
+        [Required]
+        public int  OfficeNumber { get; init; } 
+        [Required]
+        public int FloorNumber { get; init; }
+    }
+}
