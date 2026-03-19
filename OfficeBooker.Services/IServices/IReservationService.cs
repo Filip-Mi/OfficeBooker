4599622b41215b33c8df4c1dddeb60acff772219
Filip-Mi <filipmirzejewski1@gmail.com>
2026-03-19 17:52:43 +0100

    feat: add reservation service interface

---
+++ OfficeBooker.Services/IServices/IReservationService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OfficeBooker.Models.cs.DTOs;
+
+namespace OfficeBooker.Services.IServices
+{
+    public interface IReservationService
+    {
+        Task<ReservationCreateDTO> CreateReservationAsync(ReservationCreateDTO reservationCreateDTO, string userId);
+        Task<IEnumerable<ReservationCreateDTO>> GetMyReservationsAsync(string userId);
+    }
+}
