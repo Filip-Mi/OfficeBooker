8cea8c02652c61cef52b48a39261e528523b91da
Filip-Mi <filipmirzejewski1@gmail.com>
2026-03-22 18:50:43 +0100

    feature:reservation controller : GetMyReservation , CreateReservation

--- OfficeBooker/Controllers/ReservationController.cs
+++ OfficeBooker/Controllers/ReservationController.cs
@@ -1,16 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using OfficeBooker.DataAccess.Repository.I_Repository;
+using OfficeBooker.Models.cs.DTOs;
+using OfficeBooker.Services.IServices;
+using System.Security.Claims;
 
 namespace OfficeBooker.Controllers
 {
     [Route("api/[controller]")] 
     [ApiController]
+    [Authorize]
     public class ReservationController : ControllerBase
     {
-        public readonly  IUnitOfWork _unitOfWork;
-        public ReservationController(IUnitOfWork unitOfWork)
+        public readonly IReservationService _reservationService;
+        public ReservationController(IReservationService reservationService)
         {
-            _unitOfWork = unitOfWork;
+            _reservationService = reservationService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateReservation([FromBody] ReservationCreateDTO dto)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(userId)) return Unauthorized();
+
+            try
+            {
+                var result = await _reservationService.CreateReservationAsync(dto, userId);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("my")]
+        public async Task<IActionResult> GetMyReservations()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(userId)) return Unauthorized();
+
+            var reservations = await _reservationService.GetMyReservationsAsync(userId);
+            return Ok(reservations);
         }
     }
 }
