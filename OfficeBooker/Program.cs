4823c5434a4a39b6a74137fc76cfa5086ce870a5
Filip-Mi <filipmirzejewski1@gmail.com>
2026-03-23 14:24:59 +0100

    fix: initialize services before build

--- OfficeBooker/Program.cs
+++ OfficeBooker/Program.cs
@@ -19,10 +19,6 @@ namespace OfficeBooker
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
-            builder.Services.AddControllers();
-            var app = builder.Build();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -30,6 +26,7 @@ namespace OfficeBooker
             builder.Services.AddScoped<IReservationService, ReservationService>();
             builder.Services.AddScoped<IOfficeService, OfficeService>();
             builder.Services.AddScoped<IAuthService, AuthService>();
+            builder.Services.AddControllers();
             //Jwt Config
             var jwtSettings = builder.Configuration.GetSection("Jwt");
             var secretKey = jwtSettings["Key"];
@@ -52,6 +49,9 @@ namespace OfficeBooker
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey!))
                 };
             });
+            var app = builder.Build();
+       
+
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
